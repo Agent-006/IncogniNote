817416665f1c5844622bf85c9b1d2b3cbaea5c19
Sagar~ <sagargope2001@gmail.com>
2024-04-20 19:39:17 +0530

    feat(api): added ai suggestive message

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,75 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User.model";
+import {z} from "zod"
+import { usernameValidation } from "@/schemas/signUpSchema";
+
+const usernameQuerySchema = z.object({
+    username: usernameValidation
+})
+
+export async function GET(request: Request){
+        
+    await dbConnect();
+
+    try {
+        const {searchParams} = new URL(request.url)
+        const queryParam = {
+            username: searchParams.get('username')
+        }
+
+        // validate with zod
+        const result = usernameQuerySchema.safeParse(queryParam)
+        console.log(result);
+
+        if(!result.success){
+            const usernameErrors = result.error.format().username?._errors || []
+            return Response.json(
+                {
+                    success: false,
+                    message: usernameErrors?.length > 0 ? usernameErrors.join(', ') : 'Invalid query parameters',
+                },
+                {
+                    status: 400
+                }
+            )
+        }
+
+        const {username} = result.data
+
+        const existingVerifiedUser = await UserModel.findOne({username, isVerified: true})
+
+        if (existingVerifiedUser) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Username is already taken",
+                },
+                {
+                    status: 400
+                }
+            )
+        }
+        
+        return Response.json(
+            {
+                success: true,
+                message: "Username is available",
+            },
+            {
+                status: 200
+            }
+        )
+        
+    } catch (error) {
+        console.error("Error checking username", error);
+        return Response.json(
+            {
+                success: false,
+                message: "Error checking username",
+            },
+            {
+                status: 500
+            }
+        )
+    }
+}
